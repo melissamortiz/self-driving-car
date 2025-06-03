1635ae6168028e1cbc16aa0a0f7d14bd42cae832
Melissa Ortiz <melissa@greenmachine.dev>
2025-06-02 17:23:20 -0700

    Implement precision recall with brute force

--- course-2/choosing-metrics/precision_recall.py
+++ course-2/choosing-metrics/precision_recall.py
@@ -16,6 +16,31 @@ def precision_recall(ious, gt_classes, pred_classes):
     - recall [float]
     """
     # IMPLEMENT THIS FUNCTION
+    tp = 0
+    fp = 0
+    fn = 0
+    iou_threshold = 0.5
+    
+    for i in range(len(gt_classes)):
+        # Find the best predicted box for the current ground truth box.
+        best_iou_index = -1
+        max_iou = 0
+        for j in range(len(pred_classes)):
+            if ious[i, j] > max_iou and gt_classes[i] == pred_classes[j]:
+                max_iou = ious[i, j]
+                best_iou_index = j
+
+        # Determine if it's a TP or FN based on the best matched predicted box.
+        if max_iou >= iou_threshold:
+            tp += 1
+        else:
+            fn += 1
+
+    # Count FP based on predicted boxes that didn't match any ground truth box.
+    fp = len(pred_classes) - tp
+    
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / (tp + fn) if (tp + fn) > 0 else 0
     return precision, recall
 
 
